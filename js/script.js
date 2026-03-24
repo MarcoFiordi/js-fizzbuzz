645206381922e163bf7829b3c69b63515e24ad2c
Marco Fiordi <marco.fiordi97@gmail.com>
2026-03-24 15:40:59 +0100

    aggiunta logica per multipli di 3 (Fizz)

--- js/script.js
+++ js/script.js
@@ -8,8 +8,15 @@
 
 
 // ciclo da 1 a 100
-for (let i = 1; i <= 100; i++) {  
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+    }
+    else if (i % 3 === 0) {
+        console.log('Fizz')
+    }
     
-}   
-    
-    
+
+
+}
+

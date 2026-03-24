1b3cc3dffce5c034669f6f6f25e116f24c13f60d
Marco Fiordi <marco.fiordi97@gmail.com>
2026-03-24 15:19:36 +0100

    inizializzazione ciclo

---
+++ js/script.js
@@ -0,0 +1,15 @@
+'use strict'
+//1. Creare un ciclo da 1 a 100
+//2. Per ogni numero:
+//   -se è multiplo di 3 e di 5 --> stampare "FizzBuzz"
+//   -altrimenti si è multiplo di 3 --> stampare "Fizz"
+//   -altrimenti se è multiplo di 5 --> stampare "Buzz"
+//   -altrimenti stampare il numero
+
+
+// ciclo da 1 a 100
+for (let i = 1; i <= 100; i++) {  
+    
+}   
+    
+    

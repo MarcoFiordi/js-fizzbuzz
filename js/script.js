8d081e054bd582919248b1e2a488a35ca17c5761
Marco Fiordi <marco.fiordi97@gmail.com>
2026-03-24 15:47:54 +0100

    aggiunta logica per i multipli di 5 e stampa del numero nei casi restanti

--- js/script.js
+++ js/script.js
@@ -9,11 +9,19 @@
 
 // ciclo da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {  
         console.log('FizzBuzz');
     }
-    else if (i % 3 === 0) {
-        console.log('Fizz')
+    else if (i % 3 === 0) { // logica multipli di 3 
+        console.log('Fizz'); // stampa Fizz al posto dei multipli di 3
+    }
+    else if (i % 5 === 0){ // logica multipli di 5
+        console.log('Buzz'); // stampa Buzz al posto dei multipli di 5
+    }
+    // se il numero non è multiplo di 3 o di 5, stampa il numero
+    else {
+        console.log(i); // stampa il numero originale
+        
     }
     
 
